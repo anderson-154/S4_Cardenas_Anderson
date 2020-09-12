a11ac7308a051b667f4da7dad252ed4d792fabae
anderson-154 <54720103+anderson-154@users.noreply.github.com>
2020-09-12 17:43:18 -0500

    finally

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,32 +2,58 @@ package ui;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Main {
 
-	public static final String TEXT_INPUT = "data/Input2.txt";
-	public static final String TEXT_OUTPUT = "data/Output2.txt";
-	
 	public static void main(String[]args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new FileReader(TEXT_INPUT));
-		BufferedWriter bw = new BufferedWriter(new FileWriter(TEXT_OUTPUT));
-		
-		
-		String line;
 
-		while((line=br.readLine())!=null) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String line = br.readLine();
+		int n = Integer.parseInt(line);
+		DecimalFormat df = new DecimalFormat("#.0#");
+	    df.setRoundingMode(RoundingMode.FLOOR);
+
+		double round=0;
+		double count=0;
+		double imprDouble=0;
+		for(int g=0;g<n;g++) {
+			String [] parts = br.readLine().split(" ");
+			double [] nums = new double[parts.length];
 			
-			System.out.println(line);
-		
+			for(int i=0;i<parts.length;i++) {
+				nums[i] = Double.parseDouble(parts[i]);
+			}
+			
+			for(int j=nums.length-1;j>0;j--) {
+				round++;
+				for(int k=0;k<j;k++) {
+					if(nums[k]>nums[k+1]) {
+						double temp=nums[k];
+						nums[k]=nums[k+1];
+						nums[k+1]=temp;
+						count++;
+					}
+				}
+			}
+			imprDouble=count/round;
+			
+			count=0;
+			round=0;
+			String output=df.format(imprDouble)+"-";
+			for(int f=0;f<nums.length;f++) {
+				output+=nums[f]+" ";
+			}
+			bw.write(output.trim()+"\n");
 		}
-
 		
-		br.close();
 		bw.close();
+		br.close();
 	}
 }
